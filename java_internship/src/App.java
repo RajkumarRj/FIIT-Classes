a39eeaaedb60ad732cc3553b3830a216baee8226
RajkumarRj <rajpersonale@gmail.com>
2026-06-16 14:29:40 +0530

    june-16

---
+++ java_internship/src/App.java
@@ -0,0 +1,65 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+
+        // int  total = 10;
+
+        // System.out.println(total);
+        System.out.println(1);
+        System.out.println(2);
+        System.out.println(3);
+        System.out.println(4);
+
+        // control strutures 
+        // conditional statement, loops 
+
+
+        int age = 18;
+
+        // if(age > 18){
+
+        //     System.out.println("Eligible to vote");
+        // }else{
+        //     System.out.println("Not eligible to vote");
+        // }
+
+
+        // nested if 
+
+        if(age >=18){
+
+            if(true){
+                System.out.println("Eligible to vote");
+            }else{
+                System.out.println("Dont have voter id ");
+            }
+
+
+
+        }else{
+            System.out.println("less than 18 ");
+        }
+
+        // organization unique identifer
+
+        // class a to e
+
+        // ip ranges => 0 to 255
+
+        // class A => 0 - 127 
+        // class B => 128  - 191
+        // class C => 192- 223
+        // class D => 224 - 239
+        // class E => 240 - 255
+
+        
+
+
+
+
+
+
+
+
+    }
+}
