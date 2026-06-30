8a61d565653a6e7a87a4fc0e9e621652d7f6c9ed
RajkumarRj <rajpersonale@gmail.com>
2026-06-30 11:44:51 +0530

    june-30

---
+++ cc++_nl_morning/Day1.c
@@ -0,0 +1,38 @@
+#include <stdio.h>  // header file 
+
+
+int main(){
+
+    printf("Hello world \n");
+
+    // variables, data types 
+
+
+    // 1 gb => 1024 mb 
+    // 1 mb => 1024 kb
+    // 1kb => 1024 byte 
+    // 1 byte => 8 bit 
+    // 1 bit => either 0 or 1
+
+    int a = 10;  // => 4 bytes 
+
+    float b= 20;
+
+    double d = 30;
+
+    char c = 'F';
+
+    printf("%d \n" , a);
+
+    printf("%.2f \n" , b);
+
+    printf("%Lf \n" , d);
+
+    printf("%c \n" , c);
+
+
+
+
+   
+    return 0;
+}
