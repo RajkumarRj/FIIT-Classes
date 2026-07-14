90afa59d37249a2c5f2c9151c9290972cff4d8cf
RajkumarRj <rajpersonale@gmail.com>
2026-07-14 11:39:40 +0530

    july-14

--- c_c++/Exception.cpp
+++ c_c++/Exception.cpp
@@ -4,55 +4,35 @@ using namespace std;
 
 int main(){
     fstream tamil("tamil.txt" , ios::in |  ios::out);
-
     tamil<<"Hello tamil"<<endl;
     tamil<<"i5 Intel core"<<endl;
-
-
     tamil.seekg(0);
     string result;
 
     while(getline(tamil, result)){
         cout<<result<<endl;
     }
-
-
-
-
-
-
     // ofstream file("redmi.txt");
-
     // file<<"i5 intel 11th Gen"<<endl;
     // file<<"8GB RAM"<<endl;
-
-
     // ifstream file1("redmi.txt");
     // string data;
-
     // while(getline(file1, data)){
     //     cout<<data<<endl;
     // }
-    
-
     int a = 10;
     int b =0;
     cout<<"program starting"<<endl;
-
     try{
         /* code */
         throw runtime_error("RUntime error occured");
         if(b == 0) throw "Divixion by zero";
         cout<< a/0 <<endl;
-
     }
     catch(...){
         cout<<"Error occured"<<endl;
     }
-    
 
     cout<<"Program ending..."<<endl;
-
-
     return 0;
 }
---
+++ softwareDev/demo/src/App.jsx
@@ -0,0 +1,12 @@
+import Component from "./Component";
+
+function App() {
+  return (
+    <div>
+      <h1>React</h1>
+      <Component/>
+    </div>
+  );
+}
+
+export default App;
