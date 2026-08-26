ac0b5574c556c3c77f76eda62d7cc44361f419a3
RajkumarRj <rajpersonale@gmail.com>
2026-08-26 18:55:29 +0530

    html

---
+++ frontend_brindha/core_python/demo.py
@@ -0,0 +1,8 @@
+print("FIIT")
+
+a =10
+b=10
+
+print(a+b)
+
+
---
+++ puthon/demo.py
@@ -0,0 +1,13 @@
+name = "raj"
+
+if name == "rsj":
+    print("Hello there")
+else :
+    print("False")
+
+
+def add(a,b):
+    return a+b
+
+
+print(add(10,10))
