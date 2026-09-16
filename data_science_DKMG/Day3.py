c49fbc64baa0f2bb50b1944c18388fbb2951bfe5
RajkumarRj <rajpersonale@gmail.com>
2026-09-16 14:11:18 +0530

    python

---
+++ data_science_DKMG/Day3.py
@@ -0,0 +1,123 @@
+
+
+
+# sum = 0
+
+# for i in range(1,5):
+#     for j in range(1,i+1):
+#         sum +=1
+#         print(sum, end=" ")
+#     print()
+
+
+# for i in range(5,0 ,-1):
+#     for j in range(1,i+1):
+#         print(j, end=" ")
+#     print()
+
+
+
+
+# strings 
+# sequence of character enclosed with double quotes 
+
+name = "fIIT aCADEMY rEDMI mOBILE" 
+
+
+print(name.lower())
+# print(name.upper())
+
+print(name.capitalize())
+print(name.title())
+
+print(name[0: 5])
+# print(name[5:])
+# print(name[:6])
+
+# print(len(name))
+
+# print(len(name.strip()))
+
+# print(name.rstrip())
+
+# print(name.lstrip())
+
+# print(name.replace("redmi", " " ))
+
+# print(name.split(","))
+
+
+# list = ["Kaviya", "Monika", "Dharshini", "Ganesh"]
+
+# print(", ".join(list))
+
+# print(name.find("i"))
+# print(name.rfind("i"))
+
+# print(name.count("i"))
+
+
+# print(name.startswith("fiit"))
+
+# print(name.endswith("le"))
+
+
+# print(name.index("i"))
+
+# print(name.isalpha())
+# print("123px".isdigit())
+# print("123px".isalnum())
+
+# print("fiit".islower())
+# print("FIIT".isupper())
+# print(" ".isspace())
+
+print(name.swapcase())
+
+print(name)
+
+
+
+print(name)
+
+# string are immutable 
+
+# 0 1 2 
+#   -3 -2 -1
+
+print(name[-1])
+
+
+
+
+
+
+
+
+
+academy ="""
+Fiit academy 
+java course 
+data science
+"""
+
+# print(academy)
+
+
+
+
+
+# age =int(input("Enter a value"))
+
+# print(type(age))
+# print(age + 20)
+
+
+# string formating 
+
+
+fname = "Kaviya"
+age = 20
+
+print(f"My name is {fname} , and i'm {age} year old")
+
---
+++ data_science_DKMG/tempCodeRunnerFile.py
@@ -0,0 +1,10 @@
+print(academy)
+
+
+
+
+
+# age =int(input("Enter a value"))
+
+# print(type(age))
+# print(age + 20)
